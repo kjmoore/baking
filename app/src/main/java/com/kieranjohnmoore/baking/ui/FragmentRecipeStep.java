336fea9a05d451f87bbed3fa051a946431c677d9
Kieran Moore <kieranjohnmoore@gmail.com>
2019-04-26 01:33:50 +0100

    Adding media player

--- app/src/main/java/com/kieranjohnmoore/baking/ui/FragmentRecipeStep.java
+++ app/src/main/java/com/kieranjohnmoore/baking/ui/FragmentRecipeStep.java
@@ -1,12 +1,25 @@
 package com.kieranjohnmoore.baking.ui;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.kieranjohnmoore.baking.R;
 import com.kieranjohnmoore.baking.databinding.FragmentRecipeStepBinding;
 import com.kieranjohnmoore.baking.viewmodel.SharedViewModel;
@@ -21,7 +34,9 @@ import androidx.lifecycle.ViewModelProviders;
 public class FragmentRecipeStep extends Fragment {
     private static final String TAG = FragmentRecipeStep.class.getSimpleName();
 
+    private FragmentRecipeStepBinding viewBinding;
     private SharedViewModel viewModel;
+    private SimpleExoPlayer mExoPlayer;
 
     public FragmentRecipeStep() {
         // Required empty public constructor
@@ -30,21 +45,51 @@ public class FragmentRecipeStep extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final FragmentRecipeStepBinding viewBinding =
-                DataBindingUtil.inflate(inflater, R.layout.fragment_recipe_step, container, false);
+         viewBinding =
+                 DataBindingUtil.inflate(inflater, R.layout.fragment_recipe_step, container, false);
 
         viewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(SharedViewModel.class);
         viewModel.getStep().observe(this, (step) -> {
             Log.d(TAG, "Setting step: " + step);
             viewBinding.setStep(step);
+            if (step.videoURL != null && !step.videoURL.isEmpty()) {
+                final Uri videoUri = Uri.parse(step.videoURL);
+                Log.d(TAG, "Playing video: " + videoUri);
+                initializePlayer(videoUri);
+            } else {
+                viewBinding.playerView.setVisibility(View.GONE);
+            }
         });
 
         return viewBinding.getRoot();
     }
 
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(this.getContext(), trackSelector, loadControl);
+            viewBinding.playerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(this.getContext(), "Baking");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    Objects.requireNonNull(this.getContext()), userAgent),
+                    new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(false);
+        }
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+
+        if (mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
 
         viewModel.getStep().removeObservers(this);
     }
